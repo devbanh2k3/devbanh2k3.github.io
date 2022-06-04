7b8feb0d97c2b93f7c72ca3dd2f42cffb4957d49
Nguyễn Công Tiến <dev.congtien@gmail.com>
2022-06-04 19:14:32 +0700

    ful

--- Text.js
+++ Text.js
@@ -1,4 +1,4 @@
-var source = new EventSource("https://streaming-graph.facebook.com/575579367329484/live_comments?access_token=EAARVH5MvDwUBAN4nffp6InGy4HbI5Yhx7zabWySZAT1eSyMfvWn3qRNZAHLOhrHMnUZC9VjuBTDq6SBHDcS9qmf4plxZAZAtlkpnIRIXURsrISmiHNcGyYck0Hu9iyjfwZCiMn5WdmegzhglyiNwt7108u6v7McFEnX96nXAudPAkGCW59bZBI1ZBHzZB9xEUkdpIRqEBcV7kjZAZCxmqB3IyYE&comment_rate=one_per_two_seconds");
+var source = new EventSource("https://streaming-graph.facebook.com/543719004055004/live_comments?access_token=EAARVH5MvDwUBAAnYKgZC6r9BfYeZCqNt2Sw3KhFrpTC2rDy14oeNV4aZChZCx5VnNDWCTZBVm7x0BJ3LNeUiPMlmkaO0Ok5oarlvZA01eVpOFnBRzTVCr0ptOnESNGl1Md7olNHEOQEERlI1AZAArcZBATQSVIua7rfmD4ifxKFI8SdBebSVkj0ok5iCIVO8ghj0GyzP1GArNwPl0ejwXTMD&comment_rate=one_per_two_seconds");
 source.onmessage = function(event){
   // Do something with event.message for example
   const newElement = document.createElement("li");
